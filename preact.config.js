2bb72cfd3300e481fea9c449a37738c7077807bf
azulkipli <azulkipli@gmail.com>
2018-04-17 06:55:05 +0700

    update

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,3 @@
-import criticalCssPlugin from "preact-cli-plugin-critical-css";
 import asyncPlugin from "preact-cli-plugin-fast-async";
 
 // import Jarvis from "webpack-jarvis";
@@ -6,33 +5,10 @@ import asyncPlugin from "preact-cli-plugin-fast-async";
 // const preactCliSwPrecachePlugin = require("preact-cli-sw-precache");
 
 export default (config, env, helpers) => {
-  const options = {
-    // Inline the generated critical-path CSS.
-    inline: true,
-
-    // Minify critical-path CSS when inlining.
-    minify: true,
-
-    // Extract inlined styles from referenced stylesheets.
-    extract: false,
-
-    // Viewport width
-    width: 360,
-
-    // Viewport height.
-    height: 640,
-
-    // Your build directory to find css files.
-    base: path.resolve(env.dest),
-
-    // The path to a prerendered route. (e.g. index.html)
-    src: "/index.html"
-  };
   // console.log("CONFIG", JSON.stringify(config));
   // console.log("ENV", JSON.stringify(env));
 
   asyncPlugin(config);
-  criticalCssPlugin(config, env, options);
 
   // const precacheConfig = {
   //   staticFileGlobs: [
